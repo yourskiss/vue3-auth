dc18eec951a791985e73d33e9dd26ba895fa1355
K. Singh <16361374+yourskiss@users.noreply.github.com>
2025-06-18 17:01:58 +0530

    first commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Login from './../views/Login.vue';
 import Dashboard from './../views/Dashboard.vue';
 import Home from './../views/Home.vue';
@@ -29,7 +29,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history:  createWebHashHistory(), // createWebHistory(),
   routes,
 });
 
